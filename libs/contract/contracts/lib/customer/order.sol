2e3b34c8e2dac1e6ff50b44cc5eb1241873c1b3b
vlshell <vince.lynch@shell.com>
2021-11-14 09:40:57 +0000

    feat(contract): [#35] warehouse manager can accept customer order

--- libs/contract/contracts/lib/customer/order.sol
+++ libs/contract/contracts/lib/customer/order.sol
@@ -4,6 +4,7 @@ import "./credit.sol";
 
 abstract contract CustomerOrder is CustomerCredit, ProductInventory {
   struct Order {
+    uint256 idx;
     string productName;
     address customerAddress;
     bool filled;
@@ -11,9 +12,10 @@ abstract contract CustomerOrder is CustomerCredit, ProductInventory {
     string desiredDeliveryDate;
     string actualDeliveryDate;
     uint256 quantity;
+    uint256 amountOwed;
   }
-  Order[] private orders;
-  uint private orderCount = 0;
+  Order[] internal orders;
+  uint internal orderCount = 0;
 
   function newOrder(
     string memory _productName,
@@ -23,8 +25,45 @@ abstract contract CustomerOrder is CustomerCredit, ProductInventory {
   ) public {
     require(productIdExists(_productName));
     require(quantityReasonable(_productName, _quantity));
-    Order memory order = Order(_productName, msg.sender, false, _shippingAddress, _desiredDeliveryDate, 'Date Not Allocated', _quantity);
+
+    uint256 price = Products[_productName].price;
+    uint256 _amountOwed = _quantity * price;
+
+    Order memory order = Order(orderCount, _productName, msg.sender, false, _shippingAddress, _desiredDeliveryDate, 'Date Not Allocated', _quantity, _amountOwed);
     orders.push(order);
     orderCount++;
   }
+
+  function getOrderById(uint256 orderId) public view returns (
+    uint256 idx,
+    string memory productName,
+    address customerAddress,
+    bool filled,
+    string memory shippingAddress,
+    string memory desiredDeliveryDate,
+    string memory actualDeliveryDate,
+    uint256 quantity,
+    uint256 amountOwed
+  ){
+    Order memory order = orders[orderId];
+    return (order.idx, order.productName, order.customerAddress, order.filled, order.shippingAddress, order.desiredDeliveryDate, order.actualDeliveryDate, order.quantity, order.amountOwed);
+  }
+
+  function updateOrderAsFilled(uint256 orderId) internal returns (
+    uint256 idx,
+    string memory productName,
+    address customerAddress,
+    bool filled,
+    string memory shippingAddress,
+    string memory desiredDeliveryDate,
+    string memory actualDeliveryDate,
+    uint256 quantity,
+    uint256 amountOwed
+  ){
+    orders[orderId].filled = true;
+    orders[orderId].actualDeliveryDate = 'delivery date is set';
+    Order memory order = orders[orderId];
+    return (order.idx, order.productName, order.customerAddress, order.filled, order.shippingAddress, order.desiredDeliveryDate, order.actualDeliveryDate, order.quantity, order.amountOwed);
+  }
+
 }
